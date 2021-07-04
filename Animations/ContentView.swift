65617dc74fe5934e6bea8b4155832abef64ea5fe
Kevin Becker <kevin@kevinbecker.org>
2021-07-03 20:41:53 -0400

    Day 32 intro examples

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+//    @State private var animationAmount: CGFloat = 1
+    @State private var animationAmount = 0.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap Me") {
+//            self.animationAmount += 1
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                self.animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+        
+        
+//        .scaleEffect(animationAmount)
+//        .blur(radius: (animationAmount - 1 ) * 3)
+//        .animation(.interpolatingSpring(stiffness: 50, damping: 1))
+//        .animation(.easeInOut(duration: 2))
     }
 }
 
